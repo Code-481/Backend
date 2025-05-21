3dd48c869dc9de9f99d9db702d93ff9f597c43fd
wive10 <70935978+wive10@users.noreply.github.com>
2025-05-21 10:37:11 +0900

    refact: config 파일 전역화

--- src/main/java/com/deu/java/backend/config/JpaUtil.java
+++ src/main/java/com/deu/java/backend/config/JpaUtil.java
@@ -1,9 +1,8 @@
-package com.deu.java.backend.Bus.config;
+package com.deu.java.backend.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,8 +12,6 @@ public class JpaUtil {
 
     private static EntityManagerFactory buildEntityManagerFactory() {
         try {
-            Map<String, String> properties = new HashMap<>();
-
             // ENV
             Dotenv dotenv = Dotenv.configure()
                     .ignoreIfMalformed()
@@ -26,29 +23,20 @@ public class JpaUtil {
             String jdbcUser = dotenv.get("JDBC_USER");
             String jdbcPassword = dotenv.get("JDBC_PASSWORD");
 
-            System.out.println("JDBC URL: " + jdbcUrl);
-            System.out.println("JDBC USER: " + jdbcUser);
+           
 
             if (jdbcUrl == null || jdbcUser == null || jdbcPassword == null) {
                 throw new RuntimeException("필수 데이터베이스 환경 변수가 설정되지 않았습니다.");
             }
-
-            // MYSQL
-            properties.put("jakarta.persistence.jdbc.driver", "com.mysql.cj.jdbc.Driver");
+            Map<String, String> properties = new HashMap<>();
+            
             properties.put("jakarta.persistence.jdbc.url", jdbcUrl);
             properties.put("jakarta.persistence.jdbc.user", jdbcUser);
             properties.put("jakarta.persistence.jdbc.password", jdbcPassword);
 
-            // SQL
-            properties.put("hibernate.hbm2ddl.auto", "update");
-            properties.put("hibernate.show_sql", "false");
-            properties.put("hibernate.format_sql", "false");
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("418-unit", properties);
 
-            //JTA
-            properties.put("hibernate.transaction.factory_class", "org.hibernate.transaction.JDBCTransactionFactory"); // JDBC 트랜잭션 예시
-            properties.put("hibernate.transaction.jta.platform", "org.hibernate.engine.transaction.jta.platform.internal.JBossTransactionManagerLookup");
-
-            return Persistence.createEntityManagerFactory("bus-unit", properties);
+            return emf;
         } catch (Exception e) {
             System.err.println("EntityManagerFactory 초기화 중 오류 발생: " + e.getMessage());
             e.printStackTrace();
