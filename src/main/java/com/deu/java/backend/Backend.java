68ce45175f48df88cfe04ff4a2c84bcd07ea11fe
INMD1 <87979171+INMD1@users.noreply.github.com>
2025-07-07 02:57:53 +0900

    데이터 수정

--- src/main/java/com/deu/java/backend/Backend.java
+++ src/main/java/com/deu/java/backend/Backend.java
@@ -25,9 +25,6 @@ import com.deu.java.backend.Festival.service.FestivalService;
 import com.deu.java.backend.apiClient.BusanBimsApiClient;
 import jakarta.persistence.EntityManagerFactory;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class Backend {
 
     public static Javalin createApp(
@@ -72,12 +69,10 @@ public class Backend {
     public static void main(String[] args) {
         EntityManagerFactory emf = JpaUtil.getEntityManagerFactory();
 
-        // 파이썬 연동과 같은 비동기 작업을 위한 스레드 풀 생성
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
 
         // 버스 실시간 서비스
         BusanBimsApiClient apiClient = new BusanBimsApiClient();
-        BusArrivalServiceImpl busArrivalService = new BusArrivalServiceImpl(apiClient,executorService);
+        BusArrivalServiceImpl busArrivalService = new BusArrivalServiceImpl(apiClient);
         BusArrivalController busArrivalController = new BusArrivalController(busArrivalService);
 
 
